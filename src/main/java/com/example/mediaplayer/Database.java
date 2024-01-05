1cbebe01b5c2a9a69bc47992cfa6f05888c1baef
xsjens02 <simjen02@easv365.dk>
2024-01-05 11:41:36 +0100

    Added JDBC

---
+++ src/main/java/com/example/mediaplayer/Database.java
@@ -0,0 +1,75 @@
+package com.example.mediaplayer;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class Database {
+    public static final String USERNAME = "sa"; //username for SSMS login
+    public static final String PASSWORD = "1234"; //password for SSMS login
+    public static final String DATABASE_NAME = "dbTest"; //database
+    public static final String PORT = "1433"; //SSMS port here, 1433 is default
+    public static final String URL = "jdbc:sqlserver://localhost:" + PORT + ";databaseName=" + DATABASE_NAME;
+    public static final String ENCRYPT = "false";
+    public static Connection connection = null;
+
+    public void tryMe() {
+        //Setting properties
+        Properties properties = setProps();
+        //creating JDBC connection
+        connection = databaseConnection(properties, URL);
+        //doing a test with SQL-statements
+        test();
+        //closing JDBC connection
+        databaseClose(connection);
+    }
+
+    public static Properties setProps() {
+        System.out.println("Setting up properties");
+        Properties properties = new Properties();
+        properties.setProperty("user", USERNAME);
+        properties.setProperty("password", PASSWORD);
+        properties.setProperty("encrypt", ENCRYPT);
+        databaseConnection(properties, URL);
+        return properties;
+    }
+
+    public static Connection databaseConnection(Properties properties, String URL) {
+        try {
+            connection = DriverManager.getConnection(URL, properties);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+        return connection;
+    }
+
+    public static void databaseClose(Connection connection) {
+        try {
+            connection.close();
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        System.out.println("Closing connection to JDBC");
+    }
+
+    public static void test() {
+        System.out.println("Running test:");
+        //Prepare SQl-statement (CRUD)
+        PreparedStatement getData;
+        //Get data from table in database with SQl-statement
+        try {
+            getData = connection.prepareCall("SELECT * FROM tblUser");
+        } catch (SQLException e) {
+            throw new RuntimeException(e);
+        }
+        //Display table data
+        try {
+            ResultSet tableData = getData.executeQuery();
+            while (tableData.next()) {
+                int ID = tableData.getInt("fldID");
+                String name = tableData.getString("fldName");
+
+                System.out.printf("ID: %d Name: %s%n", ID, name);
+            }
+        } catch (SQLException ignore) {}
+    }
+}
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -4,6 +4,7 @@ module com.example.mediaplayer {
 
     requires org.controlsfx.controls;
     requires org.kordamp.bootstrapfx.core;
+    requires java.sql;
 
     opens com.example.mediaplayer to javafx.fxml;
     exports com.example.mediaplayer;
